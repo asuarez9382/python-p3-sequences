1acfdd21848f2b32d6249d3d20dc00b4358a1896
Adrian Suarez <asuarez9382@gmail.com>
2024-03-22 19:32:32 -0700

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    small_fib = [0,1,1,2,3,5,8,13,21]
+    if length > 9:
+        while len(small_fib) < length:
+            new_num = small_fib[-2] + small_fib[-1]
+            small_fib.append(new_num)
+        return print(small_fib[0:length])
+    else:
+        return print(small_fib[0:length])
